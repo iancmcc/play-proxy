f4226c3577c495ae4df4746c86499ad3081d986c
Ian McCracken <ian.mccracken@gmail.com>
2013-07-02 18:39:38 -0500

    Added code

---
+++ playproxy.py
@@ -0,0 +1,43 @@
+import eventlet
+requests = eventlet.import_patched('requests')
+from bs4 import BeautifulSoup
+from bottle import route, run, template, EventletServer, request
+
+PLAY = 'http://play.google.com/store/search'
+
+
+def searchPlay(search):
+    data = []
+    if not search:
+        return dict(total=len(data), data=data)
+    response = requests.get(PLAY, params={'c':'apps', 'q':search})
+    if response.status_code != 200:
+        log.warn('google play app search failed %s', search)
+        return
+
+    soup = BeautifulSoup(response.text)
+    results = soup.find_all('li', {'data-docid':True})
+    for result in results:
+        package_name = result.get('data-docid')
+        if package_name is None:
+            log.warn('package_name not found skipping app')
+            continue
+        icon_url = result.find('a', {'class':'thumbnail'}).img['src']
+        icon_url = icon_url[:-8] + '=w32'
+        title = result.find('a', {'class':'title'}).text
+        creator = result.find('a', {'class':'goog-inline-block'}).text
+        category = result.find('span', {'class': 'category'})
+        if category: category = category.a.text
+        data.append(dict(icon_url=icon_url, package_name=package_name,
+                        title=title, creator=creator, category=category))
+    return dict(total=len(data), data=data)
+
+
+@route('/query')
+def query():
+    search = request.query.get('q')
+    return searchPlay(search)
+
+
+def main():
+    run(host='0.0.0.0', port=6548, server=EventletServer)
